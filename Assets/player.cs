167f148cf7723616e650d301812767ac47e1b294
JadenKeller <jadkeller@csumb.edu>
2022-09-30 16:46:11 -0700

    Movement relative to camera

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public CharacterController controller;
     public Camera cam;
-    public Transform ptransform;
     public float sensitivity = 100f;
+
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
     private float rotation = 0f;
+
+    public float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -23,10 +29,30 @@ public class player : MonoBehaviour
         float mousex = Input.GetAxis("Mouse X");
         float mousey = Input.GetAxis("Mouse Y");
 
-        ptransform.RotateAround(ptransform.position, Vector3.up, mousex * sensitivity * Time.deltaTime);
+        transform.RotateAround(transform.position, Vector3.up, mousex * sensitivity * Time.deltaTime);
         rotation -= mousey * Time.deltaTime * sensitivity;
         rotation = Mathf.Clamp(rotation, -90f, 90f);
         cam.transform.localRotation = Quaternion.Euler(rotation, 0f, 0f);
+        
+        //player movement
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
 
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        Vector3 move = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        move = move.normalized;
+
+        playerVelocity.x = move.x;
+        playerVelocity.z = move.z;
+
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }

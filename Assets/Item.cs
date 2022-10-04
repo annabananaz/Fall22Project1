39f0fe1f299ccd5b48173b9b18bfc6fdd01ee54f
JadenKeller <jadkeller@csumb.edu>
2022-10-03 21:25:56 -0700

    Lerp item transform and cluespot logic

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Item : MonoBehaviour
 {
+    public Transform clueSpot;
+
+    private Vector3 startPos;
+    private Quaternion startRot;
+    private bool atStart = true;
+    private float lerpTime = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        startRot = transform.rotation;
+        gameObject.layer = LayerMask.NameToLayer("Item");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(lerpTime <= 1f){
+            if(atStart){
+                transform.position = Vector3.Lerp(transform.position, startPos, lerpTime);
+                transform.rotation = Quaternion.Lerp(transform.rotation, startRot, lerpTime);
+            }
+            else{
+                transform.position = Vector3.Lerp(transform.position, Player.player.hand.position, lerpTime);
+            }
+            lerpTime += Time.deltaTime;
+        }
+
+        if(!atStart){
+            RaycastHit hit;
+            if(Physics.Raycast(clueSpot.position, clueSpot.position - transform.position, out hit, 1f, 1 << LayerMask.NameToLayer("Player"))){
+                Player.player.seeClue = true;
+                print("see the thing");
+            }
+        }
+    }
+
+    public void Pickup(){
+        atStart = false;
+        lerpTime = 0f;
+    }
+
+    public void Return(){
+        atStart = true;
+        lerpTime = 0f;
     }
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player player;
+
+    public CharacterController controller;
+    public Camera cam;
+    public float sensitivity = 100f;
+
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float rotation = 0f;
+
+    public float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    //used to track if player is holding/observing item
+    private bool holding = false;
+    public bool seeClue = false;
+
+    public Transform hand;
+    private Transform heldItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        gameObject.layer = LayerMask.NameToLayer("Player");
+        player = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousex = Input.GetAxis("Mouse X");
+        float mousey = Input.GetAxis("Mouse Y");
+
+        if(Input.GetMouseButtonDown(0)){
+
+            if(holding){
+                heldItem.SendMessage("Return");
+                holding = false;
+            }
+            else{
+                RaycastHit hit;
+                if(Physics.Raycast(hand.position, hand.forward, out hit, 5f, 1 << LayerMask.NameToLayer("Item"))){
+                    heldItem = hit.transform; 
+                    heldItem.SendMessage("Pickup");
+                    holding = true;
+                }
+            }
+        }
+
+        if(holding){
+            heldItem.RotateAround(heldItem.position, transform.forward, -mousex * sensitivity * Time.deltaTime);
+            heldItem.RotateAround(heldItem.position, transform.right, mousey * sensitivity * Time.deltaTime);
+        }
+        else{
+            transform.RotateAround(transform.position, Vector3.up, mousex * sensitivity * Time.deltaTime);
+            rotation -= mousey * Time.deltaTime * sensitivity;
+            rotation = Mathf.Clamp(rotation, -90f, 90f);
+            cam.transform.localRotation = Quaternion.Euler(rotation, 0f, 0f);
+            
+            //player movement
+            groundedPlayer = controller.isGrounded;
+            if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+
+            if (Input.GetButtonDown("Jump") && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+            playerVelocity.y += gravityValue * Time.deltaTime;
+
+            Vector3 move = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+            move = move.normalized;
+
+            playerVelocity.x = move.x;
+            playerVelocity.z = move.z;
+
+            controller.Move(playerVelocity * Time.deltaTime);
+        }
+        seeClue = false;
+    }
+}
